eb8f13c49acab228d901a42b3e0ca6573efbef53
Hemanth Raj <hemanthraj@hemanthraj.in>
2023-08-21 18:26:37 +0530

    optimize further by lifting state up

--- lib/list.dart
+++ lib/list.dart
@@ -1,47 +1,52 @@
 import 'package:flutter/material.dart';
 
-class ListWidget extends StatelessWidget {
+class ListWidget extends StatefulWidget {
   const ListWidget({super.key});
+
+  @override
+  State<ListWidget> createState() => _ListWidgetState();
+}
+
+class _ListWidgetState extends State<ListWidget> {
+  var countList = List<int>.generate(100, (index) => 0);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: 100,
-      itemBuilder: (context, index) => const ListItemWidget(),
+      itemCount: countList.length,
+      itemBuilder: (context, index) => ListItemWidget(
+        count: countList[index],
+        onPressed: () => setState(() {
+          countList[index]++;
+        }),
+      ),
     );
   }
 }
 
-class ListItemWidget extends StatefulWidget {
-  const ListItemWidget({super.key});
+class ListItemWidget extends StatelessWidget {
+  final int count;
+  final VoidCallback onPressed;
 
-  @override
-  State<ListItemWidget> createState() => _ListItemWidgetState();
-}
-
-class _ListItemWidgetState extends State<ListItemWidget>
-    with AutomaticKeepAliveClientMixin {
-  var count = 0;
+  const ListItemWidget({
+    required this.count,
+    required this.onPressed,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.all(4),
-        child: Row(
-          children: [
-            Text(count.toString()),
-            MaterialButton(
-              onPressed: () {
-                setState(() {
-                  count++;
-                });
-              },
-              child: const Text("+"),
-            )
-          ],
-        ));
+      padding: const EdgeInsets.all(4),
+      child: Row(
+        children: [
+          Text(count.toString()),
+          MaterialButton(
+            onPressed: onPressed,
+            child: const Text("+"),
+          ),
+        ],
+      ),
+    );
   }
-
-  @override
-  bool get wantKeepAlive => true;
 }
